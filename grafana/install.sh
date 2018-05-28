ac4206974b3335febd5a0ea37c3c8cf192bd0e10
Felix Bauer <felix.bauer@atos.net>
2018-05-28 15:54:01 +0200

    Updated Grafana install script to use recent version + improvements on the script

--- grafana/install.sh
+++ grafana/install.sh
@@ -1,8 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash -fue
 
-wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_4.6.3_amd64.deb
+set -x
+
+# latest from https://grafana.com/grafana/download
+version=5.1.3
+test -f grafana_${version}_amd64.deb || wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${version}_amd64.deb
 sudo apt-get install -y adduser libfontconfig
-sudo dpkg -i grafana_4.6.3_amd64.deb
+sudo dpkg -i grafana_${version}_amd64.deb
 
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
@@ -11,3 +15,9 @@ sudo systemctl enable grafana-server
 grafana-cli plugins install briangann-gauge-panel
 grafana-cli plugins install grafana-piechart-panel
 service grafana-server restart
+
+echo "
+
+
+Connect to Grafana on port 3000
+login with admin:admin"
