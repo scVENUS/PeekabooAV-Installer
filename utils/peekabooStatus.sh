6ad688897be9981c65d9e4e994925ea1ca004e0e
Felix Bauer <felix.bauer@atos.net>
2018-07-03 18:58:41 +0200

    peekabooStatus: added services, db schema update, mailq command in buffer

--- utils/peekabooStatus.sh
+++ utils/peekabooStatus.sh
@@ -11,7 +11,7 @@ uname -a
 echo
 
 blue "Status of systemd units"
-for t in amavis peekaboo cuckoohttpd mongodb
+for t in amavis peekaboo cuckoohttpd mongodb postfix fetchmail grafana-server prometheus node_exporter
 do
   systemctl status $t | grep "\($t.service \|Active:\)"
 done
@@ -36,9 +36,9 @@ blue "Peekaboo DB:"
 echo "show tables" | mysql peekaboo
 
 # select analyses_time,job_hash,sample_id,sha256sum,analysis_result_v2.name,file_extension,reason from analysis_jobs_v2 join sample_info_v2 on sample_id=sample_info_v2.id join analysis_result_v2 on result_id=analysis_result_v2.id;
-echo "select count(*) as Number_of_analysed_samples from analysis_jobs_v2 join sample_info_v2 on sample_id=sample_info_v2.id join analysis_result_v2 on result_id=analysis_result_v2.id;" | mysql peekaboo
-echo "select count(*) as Number_of_unique_samples from sample_info_v2;" | mysql peekaboo
-echo "select analysis_result_v2.name,count(analysis_result_v2.name) from analysis_jobs_v2 join sample_info_v2 on sample_id=sample_info_v2.id join analysis_result_v2 on result_id=analysis_result_v2.id group by analysis_result_v2.name" | mysql peekaboo
+echo "select count(*) as Number_of_analysed_samples from analysis_jobs_v3 join sample_info_v3 on sample_id=sample_info_v3.id join analysis_result_v3 on result_id=analysis_result_v3.id;" | mysql peekaboo
+echo "select count(*) as Number_of_unique_samples from sample_info_v3;" | mysql peekaboo
+echo "select analysis_result_v3.name,count(analysis_result_v3.name) from analysis_jobs_v3 join sample_info_v3 on sample_id=sample_info_v3.id join analysis_result_v3 on result_id=analysis_result_v3.id group by analysis_result_v3.name" | mysql peekaboo
 
 echo
 user=$(grep User $unit | sed 's/.*=\(.*\)/\1/')
@@ -48,8 +48,8 @@ ls $home/malware_reports | wc -l
 du -sh $home/malware_reports
 
 echo
-blue "Mailq"
-mailq
+( blue "Mailq"
+mailq ) | more
 
 echo
 blue "Chown2me Capability"
