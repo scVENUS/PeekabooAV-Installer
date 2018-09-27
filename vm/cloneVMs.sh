9c758147d2fae0344cba27f33d0ed37650bc8bb5
Felix Bauer <felix.bauer@atos.net>
2018-09-27 17:15:38 +0200

    refactored cloneVM.sh

--- vm/cloneVMs.sh
+++ vm/cloneVMs.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
+
+function usage() {
+  echo $0 "<template VM name>"
+  echo
+  echo "Clones the template VM (arg 1) 20 times"
+}
+
+# if no argument given
+if [[ -z "$1" ]]
+then
+  echo "ERROR: no template given"
+  echo
+  usage
+  exit
+fi
+
+# help / usage
+if [[ "$1" == "-h" || "$1" == "--help" ]]
+then
+  usage
+  exit
+fi
+
+
 set -x
+templateName=$1
+namePrefix=cuckoo2
 
-for i in {2..9}; do
-
-    vboxmanage clonevm cuckoo101 --name cuckoo10${i} --snapshot CM  --options link --options keepallmacs --register
-    vboxmanage modifyvm cuckoo10${i} --macaddress1 080027E02E4${i}
-
-done
-
-for i in {0..9}; do
-
-    vboxmanage clonevm cuckoo101 --name cuckoo11${i} --snapshot CM  --options link --options keepallmacs --register
-    vboxmanage modifyvm cuckoo11${i} --macaddress1 080027E02E5${i}
-
+for i in $(seq -w 01 20)
+do
+    vboxmanage clonevm $templateName --name $namePrefix${i} --snapshot CM --options link --register || break
+    #vboxmanage modifyvm $namePrefix${i} --macaddress1 080027E02E${i}
 done
 
 
-for i in {0..9}; do
-
-    vboxmanage clonevm cuckoo101 --name cuckoo12${i} --snapshot CM  --options link --options keepallmacs --register
-    vboxmanage modifyvm cuckoo12${i} --macaddress1 080027E02E6${i}
-
-done
-
-for i in {0..9}; do
-
-    vboxmanage clonevm cuckoo101 --name cuckoo13${i} --snapshot CM  --options link --options keepallmacs --register
-    vboxmanage modifyvm cuckoo13${i} --macaddress1 080027E02E7${i}
-
-done
+echo "done. Now use snap-VM.sh to start all vms and take a running snapshot."
+echo "You might want to give it some time (some malware checks uptime to test for sandboxes)."
