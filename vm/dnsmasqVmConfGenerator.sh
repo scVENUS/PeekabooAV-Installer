54ca94441892d34e04ec0c1f84ef0b68af084133
Felix Bauer <felix.bauer@atos.net>
2018-09-27 17:16:18 +0200

    refactored dnsmasqVmConfGenerator.sh

--- vm/dnsmasqVmConfGenerator.sh
+++ vm/dnsmasqVmConfGenerator.sh
@@ -3,7 +3,10 @@
 # reqiures gnu parallel
 
 
+export namePrefix=cuckoo
 
+
+tmpFile=$(mktemp)
 
 ## CREATE DNSMASQ dhcp-host SECTION
 # list vms
@@ -15,7 +18,11 @@
 # swap fields
 # add colons to mac and output in correct format
 
-vboxmanage list vms | grep -o "cuckoo[0-9]*" | parallel vboxmanage showvminfo {} --details --machinereadable | grep "^\(name\|macaddress\)" | grep -o "\".*\"" | paste -d " "  - - | tr -d '"' | awk '{print $2 " " $1}' | sed 's/\(..\)\(..\)\(..\)\(..\)\(..\)\(..\) /dhcp-host=\1:\2:\3:\4:\5:\6,/'
+echo "########################################################"
+echo "##  dnsmasq.conf (based on registered vbox machines)  ##"
+echo "########################################################"
+
+vboxmanage list vms | grep -o "${namePrefix}[0-9]*" | parallel vboxmanage showvminfo {} --details --machinereadable | grep "^\(name\|macaddress\)" | grep -o "\".*\"" | paste -d " "  - - | tr -d '"' | awk '{print $2 " " $1}' | sed 's/\(..\)\(..\)\(..\)\(..\)\(..\)\(..\) /dhcp-host=\1:\2:\3:\4:\5:\6,/' >> $tmpFile
 
 
 
@@ -26,14 +33,24 @@ vboxmanage list vms | grep -o "cuckoo[0-9]*" | parallel vboxmanage showvminfo {}
 # filter for name
 # values without quotes only
 # output config line hostname/ip
-vboxmanage list vms | grep -o "cuckoo[0-9]*" | parallel vboxmanage showvminfo {} --details --machinereadable | grep "^name" | grep -o "cuckoo[0-9]*" | parallel echo "address=/{}/192.168.56.\$(echo {} | grep -o "[0-9]*")"
+
+vboxmanage list vms | grep -o "${namePrefix}[0-9]*" | parallel vboxmanage showvminfo {} --details --machinereadable | grep "^name" | grep -o "${namePrefix}[0-9]*" | parallel echo "address=/{}/192.168.56.\$(echo {} | grep -o "[0-9]*")" >> $tmpFile
+
+
+cat $tmpFile
 
 
 
 ## CREATE virtualbox.conf FOR cuckoo
 # from dnsmasq.conf
+
+echo
+echo "##########################################################################"
+echo "## virtualbox.conf (based on created $tmpFile (dnsmasq.conf)"
+echo "##########################################################################"
+
 function a() {
-  name=$(echo $1 | grep -o "cuckoo[0-9]*")
+  name=$(echo $1 | grep -o "${namePrefix}[0-9]*")
   ip=$(echo $1 | sed 's-.*/.*/--')
   cat <<EOF
 [$name]
@@ -45,6 +62,7 @@ resultserver_ip=192.168.56.5
 EOF
 }
 export -f a
-grep "^address=" /etc/dnsmasq.conf | parallel a {}
+grep "^address=" $tmpFile | parallel a {}
 
 
+rm $tmpFile
