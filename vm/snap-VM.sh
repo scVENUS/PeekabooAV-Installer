56e08c0abf798290e6b10de741ec0e499fc306e6
Felix Bauer <felix.bauer@atos.net>
2019-11-18 16:53:54 +0100

    Add functionality to snap-vm.sh
    snap-vm.sh now can:
    - Starts vms
    - Tests connection to Cuckoo Agent (tcp:8000)
    - Snapshots vms
    - Stops vms
    - Harden turns off clipboard and draganddrop

--- vm/snap-VM.sh
+++ vm/snap-VM.sh
@@ -1,31 +1,63 @@
 #!/bin/bash
-set -x
 
 namePrefix=cuckoo2
+ipnet=192.168.56.2
+count=10
+VBOXMANAGE="/usr/bin/vboxmanage"
 
 function usage() {
-    echo $0 "[start|snap]"
+    echo $0 "[start|test|snap|stop|harden]"
     echo
-    echo "Starts vms
+    echo "Starts vms"
+    echo "Tests connection to Cuckoo Agent (tcp:8000)"
+    echo "Snapshots vms"
+    echo "Stops vms"
+    echo "Harden turns off clipboard and draganddrop"
 }
 
-function start() {
-    for i in $(seq -w 1 20)}
+function start(){
+    for i in $(seq -w 1 $count)
     do
-        vboxmanage startvm ${namePrefix}${i} --type headless
+        $VBOXMANAGE startvm ${namePrefix}${i} --type headless
         sleep 2
     done
 }
 
-function snap() {
-    for i in {01..39}
+function stop(){
+    for i in $(seq -w 1 $count)
     do
-        vboxmanage snapshot ${namePrefix}${i} take snap1
+        $VBOXMANAGE controlvm ${namePrefix}${i} acpipowerbutton
+        sleep 2
+    done
+}
+
+function test(){
+    for i in $(seq -w 1 $count)
+    do
+        nc -vz ${ipnet}${i} 8000
+    done
+}
+
+function snap() {
+    for i in $(seq -w 1 $count)
+    do
+        $VBOXMANAGE snapshot ${namePrefix}${i} take snap1
         sleep 1
-        vboxmanage controlvm ${namePrefix}${i} poweroff
+        $VBOXMANAGE controlvm ${namePrefix}${i} poweroff
         sleep 1
-        vboxmanage snapshot ${namePrefix}${i} restorecurrent
+        $VBOXMANAGE snapshot ${namePrefix}${i} restorecurrent
         sleep 1
+    done
+}
+
+function harden() {
+    for i in $(seq -w 1 $count)
+    do
+        #$VBOXMANAGE modifyvm ${namePrefix}${i} --groups "/Productive-$d"
+        #$VBOXMANAGE modifyvm ${namePrefix}${i} --nic1 hostonly
+        #$VBOXMANAGE modifyvm ${namePrefix}${i} --hostonlyadapter1 vboxnet0
+        $VBOXMANAGE modifyvm ${namePrefix}${i} --clipboard disabled
+        $VBOXMANAGE modifyvm ${namePrefix}${i} --draganddrop disabled
     done
 }
 
@@ -38,6 +70,12 @@ fi
 case "$1" in
 "start" )
     start
+    ;;
+"stop" )
+    stop
+    ;;
+"test" )
+    test
     ;;
 "snap" )
     snap
