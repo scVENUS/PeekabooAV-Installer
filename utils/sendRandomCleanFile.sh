cccf0522a4020196bd083e133f81f14f9a4d3a96
Marcel Caspar <Clevero@users.noreply.github.com>
2020-01-07 16:27:42 +0100

    create sendRandomCleanFile.sh
    As promised, here is my script to randomly generate documents and send them directly to your mail server
    
    I use this script on an external server to test my instance for configuration and load

---
+++ utils/sendRandomCleanFile.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#   by Clevero (Marcel Caspar), Sittig Technologies GmbH
+#   28.11.2019
+#
+
+
+#
+# This script can generate some files that can be analyzed by Peekaboo.
+# In order to have every time a unique file, a random string is used to cosntruct the file.
+# Currently this scripts supports the following files:
+#       * rar with .docx inside
+#       * zip with .docx inside
+#       * docx
+#       * pdf
+#       * doc
+#
+# Setup:
+#       apt update && apt install unrar zip pandoc texlive swaks pwgen rar
+#       Replace down below the default values to yours. 
+#       * shouldpass@domain.tld (receiver)
+#       * mailserver.domain.tld (mailserver of the receiver. Might be different than the MX record)
+#       * test@mail.otherdomain.tld (sender)
+#       * mail.otherdomain.tld (HELO of the mailserver you mimmic)
+#
+#       Please also assure that you have a valid PTR/rDNS record in place. My scripts runs on a external server that is sending those emails to my mailserver at work
+#
+# Usage:
+#       sendRandomCleanfile.sh rar
+#       sendRandomCleanfile.sh docx
+
+TYPE=$1
+
+if [ $TYPE = "rar" ]; then
+        RND_NAME=$(pwgen 6 1);
+        echo $(pwgen 1024 1) > $RND_NAME.txt
+        pandoc $RND_NAME.txt -o $RND_NAME.docx
+
+        rar a $RND_NAME.rar  $RND_NAME.docx
+elif [ $TYPE = "zip" ]; then
+        RND_NAME=$(pwgen 6 1);
+        echo $(pwgen 1024 1) > $RND_NAME.txt
+        pandoc $RND_NAME.txt -o $RND_NAME.docx
+
+        zip $RND_NAME.zip  $RND_NAME.docx
+else
+        RND_NAME=$(pwgen 6 1);
+        echo $(pwgen 1024 1) > $RND_NAME.txt
+        pandoc $RND_NAME.txt -o $RND_NAME.$TYPE
+fi
+
+
+
+if [ $TYPE = "docx" ]; then
+        SAMPLE_MIME_TYPE="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+elif [ $TYPE = "pdf" ]; then
+        SAMPLE_MIME_TYPE="application/pdf"
+elif [ $TYPE = "doc" ]; then
+        SAMPLE_MIME_TYPE="application/msword"
+elif [ $TYPE = "rar" ]; then
+        SAMPLE_MIME_TYPE="application/x-rar"
+elif [ $TYPE = "zip" ]; then
+        SAMPLE_MIME_TYPE="application/zip"
+else
+        echo "no known format"
+        exit 1;
+fi
+
+echo $SAMPLE_MIME_TYPE
+
+swaks --to shouldpass@domain.tld --tls --attach-type $SAMPLE_MIME_TYPE --attach-name --attach $RND_NAME.$TYPE --suppress-data --server mailserver.domain.tld --from test@mail.otherdomain.tld --helo mail.otherdomain.tld
+rm -f $RND_NAME.$TYPE
+rm -f $RND_NAME.txt
