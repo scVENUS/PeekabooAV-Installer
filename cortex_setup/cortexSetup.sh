b65e96bec677f1b8a2d776ce66cd2acbb6ae0efe
Michael Weiser <michael.weiser@gmx.de>
2022-04-14 12:39:58 +0000

    cortex setup: Improve readability
    Improve readability by separating colouring from the messages displayed
    as well as breaking very long lines. In the case of JSON request bodies
    this adds some whitespace to the actual messages transferred which seems
    a good tradeoff for the readability gained.

--- cortex_setup/cortexSetup.sh
+++ cortex_setup/cortexSetup.sh
@@ -5,7 +5,10 @@
 
 check_last_command () {
 	if [ $? -eq 0 ]; then
-		printf " \033[38;5;118mo\033[0m\n"
+		printf " "
+		printf "\033[38;5;118m"
+		printf "o"
+		printf "\033[0m\n"
 	else
 		# switch cursor back on and reset colours before exiting
 		printf "\033[?25h\033[0m\n"
@@ -50,12 +53,14 @@ while [ "$#" -gt 0 ]; do
 done
 
 if [ -z "$CORTEX_URL" ]; then
-	echo "must specify a URL with -cu / --cortex-url (without path or trailing /, e.g. http://cortex:9001)"
+	echo "must specify a URL with -cu / --cortex-url"
+	echo "(without path or trailing /, e.g. http://cortex:9001)"
 	echo "Or set CORTEX_URL as an environment variable"
 	exit 1
 fi
 if [ -z "$ELASTIC_URL" ]; then
-	echo "must specify a URL with -eu / --elasticsearch-url (without path or trailing /, e.g. http://elasticsearch:9200)"
+	echo "must specify a URL with -eu / --elasticsearch-url"
+	echo "(without path or trailing /, e.g. http://elasticsearch:9200)"
 	echo "Or set ELASTIC_URL as an environment variable"
 	exit 1
 fi
@@ -74,7 +79,9 @@ printf "\033[?25l"
 
 CODE=$(curl -s -o /dev/null -w "%{http_code}" "$CORTEX_URL/api/job")
 RC="$?"
-printf "\033[38;5;242m$CODE\033[0m\n"
+printf "\033[38;5;242m"
+printf "$CODE"
+printf "\033[0m\n"
 
 if [ "$RC" -ne "0" ] ; then
 	echo
@@ -90,58 +97,91 @@ elif [ "$CODE" -eq "520" ]; then
 		echo "auto-generated cortex admin password: $CORTEX_ADMIN_PASSWORD"
 	fi
 
-	printf "\t\033[38;5;226mMigrate Database... \033[0m"
+	printf "\t\033[38;5;226m"
+	printf "Migrate Database... "
+	printf "\033[0m"
 	curl -f -s -o /dev/null -XPOST -H 'Content-Type: application/json' \
 		"$CORTEX_URL/api/maintenance/migrate" -d '{}'
 	sleep 3
 	check_last_command
 
-	printf "\t\033[38;5;226mMake admin user... \033[38;5;242m"
+	printf "\t\033[38;5;226m"
+	printf "Make admin user... "
+	printf "\033[38;5;242m"
 	curl -f -s -o /dev/null -XPOST -H 'Content-Type: application/json' \
 		"$CORTEX_URL/api/user" \
-		-d '{"login":"admin","name":"admin","password":"'"$CORTEX_ADMIN_PASSWORD"'","roles":["superadmin"],"organization":"cortex"}'
+		-d '{"login":"admin","name":"admin",
+			"password":"'"$CORTEX_ADMIN_PASSWORD"'",
+			"roles":["superadmin"],"organization":"cortex"}'
 	check_last_command
 
-	printf "\t\033[38;5;226mCreate organization 'PeekabooAV'... \033[38;5;242m"
+	printf "\t\033[38;5;226m"
+	printf "Create organization 'PeekabooAV'... "
+	printf "\033[38;5;242m"
 	curl -f -s -o /dev/null -XPOST -u "admin:$CORTEX_ADMIN_PASSWORD" \
-		-H 'Content-Type: application/json' "$CORTEX_URL/api/organization" \
-		-d '{ "name": "PeekabooAV", "description": "PeekabooAV organization", "status": "Active"}' 
+		-H 'Content-Type: application/json' \
+		"$CORTEX_URL/api/organization" \
+		-d '{ "name": "PeekabooAV",
+			"description": "PeekabooAV organization",
+			"status": "Active"}' 
 	check_last_command
 
-	printf "\t\033[38;5;226mCreate orgAdmin user... \033[38;5;242m"
+	printf "\t\033[38;5;226m"
+	printf "Create orgAdmin user... "
+	printf "\033[38;5;242m"
 	curl -f -s -o /dev/null -XPOST -u "admin:$CORTEX_ADMIN_PASSWORD" \
 		-H 'Content-Type: application/json' "$CORTEX_URL/api/user" \
-		-d '{ "name": "Peekaboo org Admin","password":"'"$CORTEX_ADMIN_PASSWORD"'","roles": ["read","analyze","orgadmin"], "organization": "PeekabooAV", "login": "peekaboo-admin" }'
+		-d '{ "name": "Peekaboo org Admin",
+			"password":"'"$CORTEX_ADMIN_PASSWORD"'",
+			"roles": ["read","analyze","orgadmin"],
+			"organization": "PeekabooAV",
+			"login": "peekaboo-admin" }'
 	check_last_command
-	ORG_ADMIN_KEY=$(curl -s -XPOST -u "admin:$CORTEX_ADMIN_PASSWORD" -H 'Content-Type: application/json' "$CORTEX_URL/api/user/peekaboo-admin/key/renew")
+	ORG_ADMIN_KEY=$(curl -s -XPOST -u "admin:$CORTEX_ADMIN_PASSWORD" \
+		-H 'Content-Type: application/json' \
+		"$CORTEX_URL/api/user/peekaboo-admin/key/renew")
 
-	printf "\t\033[38;5;226mCreate normal user... \033[38;5;242m"
+	printf "\t\033[38;5;226m"
+	printf "Create normal user... "
+	printf "\033[38;5;242m"
 	curl -f -s -o /dev/null -XPOST \
 		-H "Authorization: Bearer $ORG_ADMIN_KEY" \
 		-H 'Content-Type: application/json' "$CORTEX_URL/api/user" \
-		-d '{ "name": "Peekaboo", "roles": ["read","analyze"], "organization": "PeekabooAV", "login": "peekaboo-analyze" }'
+		-d '{ "name": "Peekaboo", "roles": ["read","analyze"],
+			"organization": "PeekabooAV",
+			"login": "peekaboo-analyze" }'
 	check_last_command
 
-	printf "\t\033[38;5;226mGet cortex elasticsearch index... \033[38;5;242m"
+	printf "\t\033[38;5;226m"
+	printf "Get cortex elasticsearch index... "
+	printf "\033[38;5;242m"
 	ELASTIC_INDEX=$(curl -s "$ELASTIC_URL/_search?q=_id:peekaboo-analyze" | \
 		jq -r ".hits.hits[]._index // empty")
 	if [ -z "$ELASTIC_INDEX" ] ;then
-		printf "\033[38;5;197mThere was no _index found in Elastiscsearch response\033[0m\033[?25h\n"
+		printf "\033[38;5;197m"
+		printf "There was no _index found in Elastiscsearch response"
+		printf "\033[0m\033[?25h\n"
 		exit 1
 	fi
 	check_last_command
 	printf "\t\t\033[38;5;242m"
 	echo "Index: $ELASTIC_INDEX"
 
-	printf "\t\033[38;5;226mPlace own API key in the database... \033[38;5;242m"
+	printf "\t\033[38;5;226m"
+	printf "Place own API key in the database... "
+	printf "\033[38;5;242m"
 	curl -f -s -o /dev/null -XPOST -H 'Content-Type: application/json' \
-		-d '{"doc": {"key": "'"$PEEKABOO_CORTEX_API_TOKEN"'"}}' "$ELASTIC_URL/$ELASTIC_INDEX/_update/peekaboo-analyze"
+		-d '{"doc": {"key": "'"$PEEKABOO_CORTEX_API_TOKEN"'"}}' \
+		"$ELASTIC_URL/$ELASTIC_INDEX/_update/peekaboo-analyze"
 	check_last_command
 
-	printf "\t\033[38;5;226mEnable FileInfo 8.0 Analyzer... \033[38;5;242m"
+	printf "\t\033[38;5;226m"
+	printf "Enable FileInfo 8.0 Analyzer... "
+	printf "\033[38;5;242m"
 	curl -f -s -o /dev/null -XPOST \
 		-H "Authorization: Bearer $ORG_ADMIN_KEY" \
-		-H 'Content-Type: application/json' "$CORTEX_URL/api/organization/analyzer/FileInfo_8_0" \
+		-H 'Content-Type: application/json' \
+		"$CORTEX_URL/api/organization/analyzer/FileInfo_8_0" \
 		-d '{"name": "FileInfo_8_0", "configuration": {}}'
 	check_last_command
 
@@ -149,6 +189,8 @@ elif [ "$CODE" -eq "401" ]; then
 	echo "Cortex does not need to be set-up"
 fi
 
-printf "\033[32mAll good!\033[0m\033[?25h\n"
+printf "\033[32m"
+printf "All good!"
+printf "\033[0m\033[?25h\n"
 
 exit 0
